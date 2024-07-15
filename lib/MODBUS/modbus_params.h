d018579735e0e3bfb60d984e49fad36945644d41
Michele Lombardi <michelelombardi.job@gmail.com>
2024-07-15 17:25:06 +0200

    params

--- lib/MODBUS/modbus_params.h
+++ lib/MODBUS/modbus_params.h
@@ -59,32 +59,34 @@ typedef struct
 // Definisce la dimensione massima possibile per il numero di scale
 #define MAX_SCALES 6
 
-holding_reg_params_t holding_reg_params[MAX_SCALES];
-input_reg_params_t input_reg_params;
-coil_reg_params_t coil_reg_params[MAX_SCALES];
-discrete_reg_params_t discrete_reg_params;
+extern holding_reg_params_t holding_reg_params;
+extern input_reg_params_t input_reg_params;
+extern coil_reg_params_t coil_reg_params;
+extern discrete_reg_params_t discrete_reg_params;
 
-// Parametri di comunicazione
-#define MB_PORT_NUM 2       // Numero di porta UART usata per la connessione Modbus
-#define MB_SLAVE_ADDR 1     // Indirizzo del dispositivo nella rete Modbus
-#define MB_DEV_SPEED 9600   // Velocità di comunicazione della UART
+//DEFINIZIONE PARAMETRI COMUNICAZIONE
+#define MB_PORT_NUM     2   // Number of UART port used for Modbus connection
+#define MB_SLAVE_ADDR   1     // The address of device in Modbus network
+#define MB_DEV_SPEED    9600  // The communication speed of the UART
 #define CONFIG_MB_COMM_MODE_RTU 1
 #define CONFIG_MB_UART_TXD RS485_TX
 #define CONFIG_MB_UART_RXD RS485_RX
 #define CONFIG_MB_UART_RTS RS485_EN
-
-// Indirizzi iniziali per ogni tipo di parametro Modbus
-#define HOLD_OFFSET(scale, field) ((uint16_t)(offsetof(holding_reg_params_t, field) >> 1) + (scale) * sizeof(holding_reg_params_t) / 2)
+//DEFINIZIONE START ADDRESS PER OGNI TIPO DI PARAMETRO MODBUS
+#define HOLD_OFFSET(field) ((uint16_t)(offsetof(holding_reg_params_t, field) >> 1))
 #define INPUT_OFFSET(field) ((uint16_t)(offsetof(input_reg_params_t, field) >> 1))
-#define MB_REG_COILS_START(scale) ((scale) * sizeof(coil_reg_params_t))
-#define MB_REG_HOLDING_START(scale) (HOLD_OFFSET(scale, holding_cell1MS))
+#define MB_REG_COILS_START                  (0x0000)
+#define MB_REG_HOLDING_START         (HOLD_OFFSET(holding_cell1MS))
+//DEFINIZIONE PARAMETRI PROTOCOLLO MODBUS
+#define MB_PAR_INFO_GET_TOUT                (10) // Timeout for get parameter info
+#define MB_CHAN_DATA_MAX_VAL                (6)
+#define MB_CHAN_DATA_OFFSET                 (0.2f)
+#define MB_READ_MASK                        (MB_EVENT_INPUT_REG_RD \
+                                                | MB_EVENT_HOLDING_REG_RD \
+                                                | MB_EVENT_DISCRETE_RD \
+                                                | MB_EVENT_COILS_RD)
+#define MB_WRITE_MASK                       (MB_EVENT_HOLDING_REG_WR \
+                                                | MB_EVENT_COILS_WR)
+#define MB_READ_WRITE_MASK                  (MB_READ_MASK | MB_WRITE_MASK)
 
-// Parametri del protocollo Modbus
-#define MB_PAR_INFO_GET_TOUT 10  // Timeout per ottenere le informazioni del parametro
-#define MB_CHAN_DATA_MAX_VAL 6
-#define MB_CHAN_DATA_OFFSET 0.2f
-#define MB_READ_MASK (MB_EVENT_INPUT_REG_RD | MB_EVENT_HOLDING_REG_RD | MB_EVENT_DISCRETE_RD | MB_EVENT_COILS_RD)
-#define MB_WRITE_MASK (MB_EVENT_HOLDING_REG_WR | MB_EVENT_COILS_WR)
-#define MB_READ_WRITE_MASK (MB_READ_MASK | MB_WRITE_MASK)
-
-#endif // _DEVICE_PARAMS
+#endif // !defined(_DEVICE_PARAMS)
