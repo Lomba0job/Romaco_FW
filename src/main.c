7777cd1d03b738550b46bb198a9675b28c485805
Michele Lombardi <michelelombardi.job@gmail.com>
2024-07-15 11:53:55 +0200

    pin

--- src/main.c
+++ src/main.c
@@ -1,1 +1,14 @@
+/**
+ * @file main.c
+ * @description 
+ * @date 15/07/2024
+ * @author Lombardi Michele 
+ * @copyright Nanolever 
+ * @version alpha 0.0.0
+ */
+
+#include <stdio.h>
+#include <stdint.h>
+#include <time.h>
+
 void app_main() {}
